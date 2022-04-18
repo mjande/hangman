1ac8474cc143c3a99a6df781b2e7b18fc5c13bb0
mattand <mjande@umich.edu>
2022-04-18 17:27:25 -0400

    Add save/load game

--- lib/hangman.rb
+++ lib/hangman.rb
@@ -1,3 +1,5 @@
+require 'yaml'
+
 # Class for gameplay functions like asking for input and saving game
 class Game
   attr_accessor :guesses, :win, :used_letters
@@ -5,19 +7,45 @@ class Game
 
   def initialize
     puts "Let's play Hangman!"
+    puts "Would you like to start a new game or load a previous one? (Type 'new' or 'load')."
     @computer = Computer.new
+    start_choice = gets.chomp.downcase
+    case start_choice
+    when 'new'
+      new_game
+    when 'load'
+      load_game
+    else
+      puts "Please type 'new' or 'load' to begin."
+      new
+    end
+  end
+
+  def new_game
     @guesses = 6
     @win = false
     @used_letters = []
-    play_round until win == true || guesses == 0
+    play_round until win == true || guesses.zero?
+  end
+
+  def load_game
+    saved_game = File.open('saved_game.yml', 'r')
+    data = YAML.load(saved_game)
+    @guesses = data[:guesses]
+    @used_letters = data[:used_letters]
+    computer.solution = data[:solution]
+    computer.partial_solution = data[:partial_solution]
+    @win = false
+    play_round until win == true || guesses.zero?
   end
 
   def play_round
     puts "\nYou have #{guesses} guesses remaining."
     computer.display_solution_partial
-    puts "What is your next guess? (You may enter a letter
-    or a whole word for your guess.)"
+    puts 'What is your next guess?'
+    puts "(You may enter a letter or a whole word for your guess, \nor type 'exit' to save and quit.)"
     guess = gets.chomp.downcase
+    save_game if guess == 'exit'
     return unless letter_check(guess)
 
     result = computer.check_guess(guess)
@@ -46,6 +74,25 @@ class Game
       puts 'You already tried that one! Try again!'
       false
     end
+  end
+
+  # What needs to saved/serialized: guesses, used_letters, solution (computer),
+  # partial_solution(computer)
+  def serialize
+    YAML.dump({
+      guesses: @guesses,
+      used_letters: @used_letters,
+      solution: computer.solution,
+      partial_solution: computer.partial_solution
+    })
+  end
+
+  def save_game
+    saved_game = File.new('saved_game.yml', 'w')
+    data = serialize
+    saved_game.write(data)
+    saved_game.close
+    exit
   end
 end
 
@@ -93,7 +140,7 @@ class Computer
   def replace_letters(letter)
     letter_index = solution.chars.each_index.select { |i| solution[i] == letter }
     letter_index.each { |i| partial_solution[i] = "#{letter} " }
-    if partial_solution.join == solution
+    if partial_solution.join.delete(' ') == solution
       'win'
     else
       true
